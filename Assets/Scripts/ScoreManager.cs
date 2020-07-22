0d1c46820e3d47ef42e495eaf174505fbe67ae35
patrickpr86 <sh4dbr@gmail.com>
2020-07-22 13:33:37 -0300

    test finished

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+	private Board board;
+	public Text scoreText;
+	public int score;
+	public Image scoreBar;
+
+	// Use this for initialization
+	void Start () {
+		board = FindObjectOfType<Board>();
+		UpdateBar();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoreText.text = "" + score;
+	}
+
+    public void IncreaseScore(int amountToIncrease)
+	{
+		score += amountToIncrease;
+		UpdateBar();
+	}
+
+    private void UpdateBar()
+	{
+		if (board != null && scoreBar != null)
+        {
+			
+            int length = board.scoreGoals.Length;
+          
+			scoreBar.fillAmount = (float)score / (float)board.scoreGoals[length - 1];
+
+
+        }
+	}
+}
---
+++ Assets/Scripts/TimeCount.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class TimeCount : MonoBehaviour { 
+
+    public Text timeText;
+    public float time = 0;
+    public float amountTime = 120;
+
+    public bool finalTime ;
+
+
+
+    // Start is called before the first frame update
+    void Start()    {
+        time = amountTime;
+    }
+
+    // Update is called once per frame
+    void Update()    {
+       timer();
+    }
+
+    void timer(){
+        if(!finalTime ){
+
+            time -= Time.deltaTime;
+            print(time);
+            TimeSpan timeSpan = TimeSpan.FromSeconds(time);
+            timeText.text = string.Format("{1:D2}:{2:D2}", timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+            if(time <= 0){
+                finalTime = true;
+                //chamar função para proximo level
+            }
+        }
+
+        
+
+    }
+
+
+}
